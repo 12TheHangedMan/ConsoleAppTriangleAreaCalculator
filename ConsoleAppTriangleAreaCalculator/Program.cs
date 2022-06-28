519550e7bb5fa1c3dcf307f0dbe79f0935f334c6
TOIRAHCEHT70 <TOIRAHCEHT70@DESKTOP-PVIROVV>
2022-06-28 01:36:59 -0700

    添加项目文件。

---
+++ ConsoleAppTriangleAreaCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+TriangleArea NewTriangle = new TriangleArea(3, 4, 5);
+NewTriangle.Qinjiushao();
+Console.WriteLine(NewTriangle.area);
+
+class TriangleArea
+{
+    double a, b, c;
+    public double area { get; private set; }
+    public TriangleArea(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public TriangleArea(double b, double h)
+    {
+        area = b * h;
+    }
+
+    public void Qinjiushao()
+    {
+        area = Math.Sqrt(this.a * this.a * this.c * this.c * 4 - Math.Pow(this.a * this.a +this.c * this.c - this.b * this.b, 2)) / 4;
+    }
+}
+
